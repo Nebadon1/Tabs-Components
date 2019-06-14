51a3115a39da3a12e29ba85a9d6bf00149502b07
Carlos Mitchell <kakismitchell@gmail.com>
2019-06-14 01:26:09 -0400

    MVP 3

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -3,7 +3,7 @@ class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
      this.elementProperty = element;
-    //console.log(this.elementProperty.dataset.tab);
+    
 
     // Get the custom data attribute on the Link
      this.data = this.elementProperty.dataset.tab;
@@ -32,14 +32,15 @@ class TabLink {
     // Get all of the elements with the tabs-link class
 
     const links = document.querySelectorAll('.tabs-link');
-
+ 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
 
     Array.from(links).forEach(link =>{
       link.classList.remove('tabs-link-selected');
-    
+
     });
 
+    
     
 
     // Add a class named "tabs-link-selected" to this link
@@ -58,18 +59,21 @@ class TabItem {
     // Assign this.element to the passed in element
 
     this.element = element;
+   
+
   }
 
   select() {
     // Select all ".tabs-item" elements from the DOM
 
-    const items = document.querySelectorAll(".tab-item")
+    const items = document.querySelectorAll(".tabs-item")
 
 
     // Remove the class "tabs-item-selected" from each element
 
-    items.forEach( item => {
+    items.forEach( item =>{
       item.classList.remove('tabs-item-selected');
+     console.log(item.classList);
     })
     // Add a class named "tabs-item-selected" to this element
 
